fd9fcfb4b23f99ff49a73b03193791c41bb7c757
ravivir <rvkmrrahangdale@gmail.com>
2026-07-31 00:00:36 +0530

    This is second commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-Hello from Ravi 
+// script.js
+
+console.log("Welcome to JavaScript!");
+
+function showMessage() {
+    alert("Hello, World!");
+}
+
+showMessage();Hello from Ravi 
 This is js file
 
